e20b53c8f4034a7299a903be36071eea87d0ab21
Max Ovdiyenko <4max.ovd@gmail.com>
2025-07-04 23:35:17 -0400

    changed env variable in front end to include necessary REACT_APP prefix

--- client/src/lib/axiosClient.js
+++ client/src/lib/axiosClient.js
@@ -8,7 +8,7 @@ api.interceptors.request.use(async (config) => {
     const token = session?.access_token;
 
     if (token) {
-        config.baseURL = process.env.DATABSE_URL;
+        config.baseURL = process.env.REACT_APP_DATABSE_URL;
         config.headers.Authorization = `Bearer ${token}`;
     }
 
